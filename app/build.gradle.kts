8bed1ab51b68250764439f490a6b64d7bc941533
Gregoire L. D. <g@duval.paris>
2026-06-03 12:18:26 +0200

    French fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.duval.sesamelite"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.4.2"
+        versionCode = 15
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
