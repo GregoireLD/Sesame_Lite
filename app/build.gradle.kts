dd8c3e7302d8ca867c186e2562ac8f63240eca12
Gregoire L. D. <g@duval.paris>
2026-07-01 10:55:11 +0200

    Empty link fix 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.paris.duval.sesamelite"
         minSdk = 26
         targetSdk = 36
-        versionCode = 27
-        versionName = "1.5.2"
+        versionCode = 28
+        versionName = "1.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
